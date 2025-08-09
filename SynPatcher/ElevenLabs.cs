fade1c63f357c0532a40eec60c6a517418553f45
Mini <mini@minizbot.org>
2025-08-09 06:13:32 -0400

    first commit

---
+++ SynPatcher/ElevenLabs.cs
@@ -0,0 +1,37 @@
+namespace SynPatcher;
+public enum LogMode
+{
+    NONE, NORMAL, DEBUG
+}
+public class ElevenLabs
+{
+    public string key = "";
+    public string voice_id = "";
+    public string model_id = "eleven_flash_v2_5";
+    public VoiceSettings voice_settings = new();
+    public bool dry_run = true;
+    public LogMode log_mode = LogMode.NORMAL;
+    public HashSet<string> player_names = [];
+}
+
+struct Request(string t, ElevenLabs apiinfo)
+{
+    public string text = t;
+    public VoiceSettings voice_settings = apiinfo.voice_settings;
+    public string model_id = apiinfo.model_id;
+}
+public class VoiceSettings
+{
+    public VoiceSettings()
+    {
+        stability = 0.5;
+        similarity_boost = 0.75;
+        style = 0.5;
+        use_speaker_boost = false;
+    }
+    public double speed = 1.0;
+    public double stability;
+    public double similarity_boost;
+    public double style;
+    public bool use_speaker_boost;
+}
---
+++ SynPatcher/Files.cs
@@ -0,0 +1,18 @@
+using Mutagen.Bethesda.Plugins;
+using NAudio.Wave;
+namespace SynPatcher;
+public class VoiceLine
+{
+    public HashSet<FormKey> forms = [];
+    public string guid = string.Empty;
+    public ulong splen = 0;
+}
+
+public static class Exts
+{
+    public static ulong GetMp3Duration(string fileName)
+    {
+        using var mp3Reader = new Mp3FileReader(fileName);
+        return (ulong)mp3Reader.TotalTime.TotalMilliseconds;
+    }
+}
