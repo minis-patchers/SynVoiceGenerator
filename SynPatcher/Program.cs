770cf79647ef0cc50440dac17b63977582bd8662
Mini <mini@minizbot.org>
2026-02-21 17:41:56 -0500

    Logging and empty variant removal

--- SynPatcher/Program.cs
+++ SynPatcher/Program.cs
@@ -175,6 +175,10 @@ public static class Program
                 Log($"{ex.Message}", LogMode.NORMAL);
             }
         }
+        var rem = lines.Where(x => x.Value.variants.Count == 0).Select(x => x.Key).ToHashSet();
+        rem.ForEach(x => lines.Remove(x));
+        Console.WriteLine($"Removed {rem.Count} objects with no variants");
+        Console.WriteLine($"Writing {lines.Count} items for VPC-SKSE");
         var files = lines.SelectMany(x => x.Value.forms).Select(x => x.ModKey.ToString()).Distinct().ToHashSet();
         Directory.CreateDirectory(Path.Join(state.DataFolderPath, "Sound", "VPC", "DefaultVoice", "Data"));
         foreach (var fil in files)
