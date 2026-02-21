ac7d41d2e83e1a8517e7f57eaea4a5a809865b29
Mini <mini@minizbot.org>
2026-02-21 04:45:12 -0500

    Cleanups

--- SynPatcher/VoiceSystem.cs
+++ SynPatcher/VoiceSystem.cs
@@ -4,18 +4,22 @@ public enum LogMode
 {
     NONE, NORMAL, DEBUG
 }
-public class ElevenLabs
+
+public class APIConfig
 {
     public string lang = "en";
-    public bool dry_run = true;
+    public string voice = "Glinda";
     public LogMode log_mode = LogMode.NORMAL;
     public Dictionary<string, HashSet<string>> replacementLists = new(){
-        {"Alias=Player", ["Vrel"]}
+        {"Alias=Player", []},
+        {"Alias=CurrentRelic", ["Silver Arrow", "Leviathan Diamond"]}
     };
 }
 
-struct Request(string t, ElevenLabs apiinfo)
+struct Request(string t, string salt, APIConfig apiinfo)
 {
     public string text = t;
     public string lang = apiinfo.lang;
+    public string voice = apiinfo.voice;
+    public string salt = salt;
 }
