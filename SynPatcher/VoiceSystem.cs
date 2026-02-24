7537bd6eea1e659de1ce07a1e8f6ea486fb3ead1
Mini <mini@minizbot.org>
2026-02-23 23:42:25 -0500

    Update VoiceSystem.cs

--- SynPatcher/VoiceSystem.cs
+++ SynPatcher/VoiceSystem.cs
@@ -9,6 +9,7 @@ public class APIConfig
 {
     public string lang = "en";
     public string voice = "Glinda";
+    public int seed = 123;
     public string api_server = "http://localhost:8000";
     public LogMode log_mode = LogMode.NORMAL;
     public int print_every_gen = 1000;
@@ -23,4 +24,5 @@ struct Request(string t, APIConfig apiinfo)
     public string text = t;
     public string lang = apiinfo.lang;
     public string voice = apiinfo.voice;
+    public int seed = apiinfo.seed;
 }
