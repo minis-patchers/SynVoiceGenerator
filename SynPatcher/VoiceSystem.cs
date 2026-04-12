d3e763c6d5f6b38a67a622996dfb3c4cc967733e
Mini <mini@minizbot.org>
2026-04-12 19:17:46 -0400

    Expose API server parameters (with my config)

--- SynPatcher/VoiceSystem.cs
+++ SynPatcher/VoiceSystem.cs
@@ -7,8 +7,7 @@ public enum LogMode
 
 public class APIConfig
 {
-    public string lang = "English";
-    public string voice = "Regina";
+    public VoiceSettings VoiceConfiguration = new();
     public string api_server = "http://localhost:8000";
     public int iterations = 1;
     public LogMode log_mode = LogMode.NORMAL;
@@ -18,9 +17,23 @@ public class APIConfig
     };
 }
 
+public class VoiceSettings
+{
+    public string lang = "English";
+    public string voice = "Regina";
+    public float temperature = 0.4f;
+    public int top_k = 50;
+    public float top_p = 0.8f;
+    public float rep_penalty = 1.1f;
+}
+
 struct Request(string t, APIConfig apiinfo)
 {
     public string text = t;
-    public string lang = apiinfo.lang;
-    public string voice = apiinfo.voice;
+    public string lang = apiinfo.VoiceConfiguration.lang;
+    public string voice = apiinfo.VoiceConfiguration.voice;
+    public float temperature = apiinfo.VoiceConfiguration.temperature;
+    public int top_k = apiinfo.VoiceConfiguration.top_k;
+    public float top_p = apiinfo.VoiceConfiguration.top_p;
+    public float rep_penalty = apiinfo.VoiceConfiguration.rep_penalty;
 }
